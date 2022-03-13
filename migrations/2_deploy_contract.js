e0ba17d9e6f20f0a1d72799c4b8f350c5e0c781a
angel900919 <rambal90au@gmail.com>
2022-03-13 17:30:04 +1000

    TokenWorking2

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,5 @@
 var SampleToken = artifacts.require("SampleToken");
 
 module.exports = function(deployer) {
-    deployer.deploy(SampleToken, "UdacityExampleToken", "UET", 18, 1000);
+    deployer.deploy(SampleToken, "UdacityExampleToken", "UET", 18, 1000); // reduce 18 to get more tokens
 };
